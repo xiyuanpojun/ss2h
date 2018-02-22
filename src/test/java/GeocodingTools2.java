5e072e854264dd3d5407e89975285c725174fe59
zhangnaiping <2906951747@qq.com>
2018-02-22 09:45:08 +0800

    1.用户地址修改2

---
+++ src/test/java/GeocodingTools2.java
@@ -0,0 +1,143 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+public class GeocodingTools2 {
+    /**
+     * 启动程序
+     *
+     * @param args
+     * @throws SQLException
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws SQLException, InterruptedException {
+        DistUpdate();
+    }
+
+    // 修改数据库用户档案表的LNG,LAT,DIST-CT属性
+    private static void DistUpdate() throws SQLException {
+        // 多个表
+        String[] tables = {"USER_CBJF", "USER_GDZL", "USER_GZBX", "USER_YKBZ_DY", "USER_YKBZ_GY", "USER_YYTFW"};
+        for (String table : tables) {
+            // 修改地址
+            update(table);
+
+        }
+    }
+
+    /**
+     * 获取数据库中的所有地点
+     *
+     * @param table
+     * @return
+     * @throws SQLException
+     */
+    public static void update(String table) throws SQLException {
+        Connection con = DataSource.getCon();
+        PreparedStatement pre, pre2;
+        ResultSet result;
+        String sql, sql2;
+
+        if (table.equals("USER_GZBX")) {
+            sql = "SELECT ORG,PROV,YDDZ FROM USER_GZBX";
+            sql2 = "UPDATE USER_GZBX SET ADDR_CODE = ? , LNG = ? , LAT = ? ,DIST_CT = ? WHERE YDDZ = ?";
+        } else {
+            sql = "SELECT CITY,PROV,YDDZ FROM " + table;
+            sql2 = "UPDATE " + table + " SET ADDR_CODE = ? , LNG = ? , LAT = ? ,DIST_CT = ? WHERE YDDZ = ?";
+        }
+        pre = con.prepareStatement(sql);
+        result = pre.executeQuery();
+        while (result.next()) {
+            pre2 = con.prepareStatement(sql2);
+            TCityLocationEntity2 entity2 = getLatAndLngByAddress(
+                    result.getString(2) + "-" + result.getString(1) + "-" + result.getString(3));
+            // 获得市中心的经纬度
+            TCityLocationEntity2 centralcity = getLatAndLngByAddress(result.getString(2) + "-" + result.getString(1));
+            // 计算地址到市中心距离
+            Double distance = MapUtil.getDistance(entity2.getLng(), entity2.getLat(), centralcity.getLng(),
+                    centralcity.getLat());
+            pre2.setString(1, entity2.getCode());
+            pre2.setDouble(2, entity2.getLng());
+            pre2.setDouble(3, entity2.getLat());
+            pre2.setDouble(4, distance);
+            pre2.setString(5, result.getString(3));
+//			 if( pre2.executeUpdate()>=1) {
+//			 System.out.println("修改成功"+result.getString(2)+"-"+result.getString(1)+"-"+result.getString(3)+"距离："+distance);
+//			 }
+//			 else {
+//			 System.out.println("修改失败");
+//			 }
+            pre2.close();
+        }
+
+        result.close();
+        pre.close();
+        con.close();
+    }
+
+    /**
+     * 获取地点经纬度信息
+     *
+     * @param addr
+     * @return
+     */
+    public static TCityLocationEntity2 getLatAndLngByAddress(String addr) {
+        TCityLocationEntity2 entity = null;
+        String address = "";
+        String ak = "xh2XppvAc5uB36HDZHKTOMnV3gSUULTb";
+        try {
+            address = java.net.URLEncoder.encode(addr, "UTF-8");
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }
+        String url = String.format("http://api.map.baidu.com/geocoder/v2/?ak=" + ak + "&output=json&address=%s",
+                address);
+        URL myURL = null;
+        URLConnection httpsConn;
+        // 进行转码
+        try {
+            myURL = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            assert myURL != null;
+            httpsConn = myURL.openConnection();
+            if (httpsConn != null) {
+                InputStreamReader insr = new InputStreamReader(httpsConn.getInputStream(), "UTF-8");
+                BufferedReader br = new BufferedReader(insr);
+                String data;
+                if ((data = br.readLine()) != null) {
+                    JSONObject object = JSON.parseObject(data);
+                    entity = new TCityLocationEntity2();
+                    if (object.getString("status").equals("0")) {
+                        entity.setCode("1");
+                        object = object.getJSONObject("result").getJSONObject("location");
+                        entity.setLng(object.getDouble("lng"));
+                        entity.setLat(object.getDouble("lat"));
+                    } else {
+                        entity.setCode("0");
+                    }
+                }
+                insr.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return entity;
+    }
+
+}
