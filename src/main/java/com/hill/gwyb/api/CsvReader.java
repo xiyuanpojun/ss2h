1fa75b3c624033202170e6fc4598f70babe14c3e
梁强 <173228514@qq.com>
2018-02-26 13:53:02 +0800

    1.整合项目

--- src/main/java/com/hill/gwyb/api/CsvReader.java
+++ src/main/java/com/hill/gwyb/api/CsvReader.java
@@ -9,39 +9,40 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CsvReader {
-	/**
+    /**
      * 导入
-     * 
+     *
      * @param file csv文件(路径+文件)
      * @return
      */
-    public static List<List> importCsv(File file){
-        List<List> dataList=new ArrayList<List>();
-        
-        BufferedReader br=null;
-        try { 
+    public static List<List> importCsv(File file) {
+        List<List> dataList = new ArrayList<List>();
+
+        BufferedReader br = null;
+        try {
             br = new BufferedReader(new FileReader(file));
-            String line = ""; 
-            int i=0;
+            String line = "";
+            int i = 0;
             while ((line = br.readLine()) != null) {
-            	i++;
-            	if(i==1){
-            		continue;
-            	}
+                i++;
+                if (i == 1) {
+                    continue;
+                }
                 dataList.add(Arrays.asList(line.split(",")));
             }
-        }catch (Exception e) {
-        }finally{
-            if(br!=null){
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
                 try {
                     br.close();
-                    br=null;
+                    br = null;
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
- 
+
         return dataList;
     }
 }
